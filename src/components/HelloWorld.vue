4b109d98420e71ab14d08f99cbc27a16a9261580
Zubair Shibly <zubairshibly08@gmail.com>
2024-12-05 22:43:56 -0500

    Updated HelloWorld.vue with deployment instructions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,8 +18,32 @@
     <h2>Difficulties Encountered</h2>
     <p>
       One difficulty I encountered was configuring the project to work with
-      GitHub Pages. I solved this by following the official Vue documentation on
-      deploying to GitHub Pages.
+      GitHub Pages. Initially, I faced issues with the deployment process and
+      ensuring that the changes were reflected on the GitHub Pages site.
+    </p>
+    <p>
+      To deploy to GitHub Pages, I followed these steps:
+      <ol>
+        <li>First, I installed the <code>gh-pages</code> package by running <code>npm install gh-pages --save-dev</code>.</li>
+        <li>Then, I added the following scripts to my <code>package.json</code> file:
+          <pre><code>
+"scripts": {
+  "deploy": "vue-cli-service build && gh-pages -d dist"
+}
+          </code></pre>
+        </li>
+        <li>Next, I configured the <code>vue.config.js</code> file to set the correct public path:
+          <pre><code>
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-vue-project/'
+    : '/'
+}
+          </code></pre>
+        </li>
+        <li>Finally, I ran the deployment script using <code>npm run deploy</code>.</li>
+      </ol>
+      Following these steps ensured that my changes were successfully deployed and visible on GitHub Pages.
     </p>
   </div>
 </template>
